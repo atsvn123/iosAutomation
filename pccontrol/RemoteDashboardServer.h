0d60af347ff98d925b32f19729b112ddd62c847d
Epic0001 <110989057+Epic0001@users.noreply.github.com>
2026-07-09 18:24:58 -0400

    Link persistent dashboard entry point

--- pccontrol/RemoteDashboardServer.h
+++ pccontrol/RemoteDashboardServer.h
@@ -1,6 +1,14 @@
 #ifndef ZX_REMOTE_DASHBOARD_SERVER_H
 #define ZX_REMOTE_DASHBOARD_SERVER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void ZXDashboardReloadConfiguration(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
